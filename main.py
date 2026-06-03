652d6cf32d2c6125b7109c6712a3c1f89cff1df0
Siddharth Rai <135386624+Sidd-Rai@users.noreply.github.com>
2026-06-04 00:35:49 +0530

    Added streaming animation to the responses

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 from google import genai
 import streamlit as st
+import time
 
 GEMINI_MODEL_NAME = "gemini-3.1-flash-lite"
-def generateContent(message):
-    if(message):
-        if len(message)<3:
-            st.text("Message too short!")
+
+def streamContent(message):
+    if message:
+        if len(message) < 3:
+            st.error("Message too short!")
         else:
             client = genai.Client()
-            response = client.models.generate_content(
-                model = GEMINI_MODEL_NAME,
+            response = client.models.generate_content_stream(
+                model=GEMINI_MODEL_NAME,
                 contents=message
-                )
-            st.text(response.text)
-            st.text("Output generated") 
+            )
+            
+            placeholder = st.empty()
+            full_text = ""
+            
+            for chunk in response:
+                if chunk.text:
+                    words = chunk.text.split(" ")
+                    for index,word in enumerate(words):
+                        full_text += word
+                        if index < len(words) - 1:
+                            full_text += " "
+                        placeholder.markdown(full_text + "▌")   
+                        time.sleep(0.05)     
+            placeholder.markdown(full_text)
+            st.caption("Output generated")  
     return
 
 st.title("ChatGippity")
 st.header("Start texting now!")
-message = st.text_input("(Currently 'G' in \"Gippity\" stands for \"Gajni\", basically it forgets everything you've said in the previous messages)")
-generateContent(message)
+st.subheader("Type here bruh!")
+
+message = st.text_input(
+    label='(Currently \'G\' in "Gippity" stands for "Gajni", basically it forgets everything you\'ve said in the previous messages)',
+    key="user_input"
+)
+
+if message:
+    streamContent(message)
